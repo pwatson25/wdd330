b624ba6d7c2a47d8f990042f6d02fbfa5937736b
peterjwatson <peterskyguy@gmail.com>
2022-01-08 14:55:55 -0500

    try try again

---
+++ js/prophets.js
@@ -0,0 +1,25 @@
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const links = jsonObject['links'];
+    const weeklyLinks = document.querySelector('.weeklyLinks');
+
+    links.forEach(link => {
+        let li = document.createElement('li');
+        let newUrl = document.createElement('a');
+
+
+        new.setAttribute('href', link.url);
+       
+        card.append(newUrl);
+
+        cards.append(li);
+    });
+  });
+
+  
