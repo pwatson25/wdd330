53b72655aa8398cb5767038f5246133b85370127
peterjwatson <peterskyguy@gmail.com>
2022-01-15 22:16:05 -0500

    push

---
+++ js/week2.js
@@ -0,0 +1,27 @@
+const requestURL = 'https://pwatson25.github.io/wdd330/notes-and-assignments.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const links = jsonObject['week2'];
+    const assignmentLinks = document.querySelector('.assignmentLinks');
+
+    links.forEach(link => {
+      const li = document.createElement('li');
+      const anchor = document.createElement('a');
+      const anchorText = document.createTextNode(`${link.label}`);
+     
+
+      anchor.setAttribute('href', link.url);
+
+       
+      anchor.appendChild(anchorText);
+      li.append(anchor);
+      assignmentLinks.appendChild(li);
+    });
+  });
+
+  
