0918a21cf7f4501bfc041e3286b6e3f88c0b0669
peterjwatson <peterskyguy@gmail.com>
2022-04-06 10:32:34 -0400

    suck on them eggs

---
+++ js/linksNav.js
@@ -0,0 +1,25 @@
+const requestWeeklyLinksURL = 'https://pwatson25.github.io/wdd330/linksNav.json';
+
+fetch(requestWeeklyLinksURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const week = jsonObject['week'];
+    const weeklyLinks = document.querySelector('.weeklyLinks');
+    console.log(week);
+
+    week.forEach(week => {
+      const li = document.createElement('li');
+      const anchor = document.createElement('a');
+      const anchorText = document.createTextNode(`${week.label}`);
+
+
+      anchor.setAttribute('href', week.url);
+
+
+      anchor.appendChild(anchorText);
+      li.append(anchor);
+      weeklyLinks.appendChild(li);
+    });
+  });
